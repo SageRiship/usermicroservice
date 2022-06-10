24e781eb48dfa88e194220a46903559f3dc9e6e0
SageRiship <92663104+SageRiship@users.noreply.github.com>
2022-06-10 13:31:43 +0530

    Add files via upload

---
+++ encryption/encryptDecrypt.go
@@ -0,0 +1,68 @@
+package encryption
+
+import (
+	"crypto/aes"
+	"crypto/cipher"
+	"encoding/base64"
+	"log"
+)
+
+var bytes = []byte{35, 46, 57, 24, 85, 35, 24, 74, 87, 35, 88, 98, 66, 32, 14, 05}
+
+// This should be in an env file in production
+const MySecret string = "abc&1*~#^2^#s0^=)^^7%b34"
+
+func Encode(b []byte) string {
+	return base64.StdEncoding.EncodeToString(b)
+}
+
+func Decode(s string) []byte {
+	data, err := base64.StdEncoding.DecodeString(s)
+	if err != nil {
+		panic(err)
+	}
+	return data
+}
+
+// Encrypt method is to encrypt or hide any classified text
+func Encryption(text, MySecret string) (string, error) {
+	block, err := aes.NewCipher([]byte(MySecret))
+	if err != nil {
+		return "", err
+	}
+	plainText := []byte(text)
+	cfb := cipher.NewCFBEncrypter(block, bytes)
+	cipherText := make([]byte, len(plainText))
+	cfb.XORKeyStream(cipherText, plainText)
+	return Encode(cipherText), nil
+}
+
+func Decryption(text, MySecret string) (string, error) {
+	block, err := aes.NewCipher([]byte(MySecret))
+	if err != nil {
+		return "", err
+	}
+	cipherText := Decode(text)
+	cfb := cipher.NewCFBDecrypter(block, bytes)
+	plainText := make([]byte, len(cipherText))
+	cfb.XORKeyStream(plainText, cipherText)
+	return string(plainText), nil
+}
+
+func Encrypt(text string) string {
+	res, err := Encryption(text, MySecret)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	return res
+}
+
+func Decrypt(text string) string {
+	res, err := Decryption(text, MySecret)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	return res
+}
